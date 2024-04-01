95ffd3cbdd1d5d662872f1284965cbdf4932f6c2
Natanael27 <alderetenatanael27@gmail.com>
2024-04-01 18:07:16 -0300

    carga de la creacion del proyecto ionic

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'main',
+    loadComponent: () => import('./main/main.page').then( m => m.MainPage)
+  },
+];
